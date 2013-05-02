66e1b82f012e694d0d83ab04968374c741d5483d
YannickMathey <hello@yannickmathey.com>
2013-05-02 09:39:30 +0200

    ajout/modification du glyphe i sans string dans l'array

--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -1,15 +1,15 @@
 angular.module('prototyp0Filters', ['prototyp0App', 'prototyp0Glyph'])
 	.filter('glyph', function( $compile, $rootScope, glyphs ) {
 		return function( input, glyph ) {
-			var d = [];
+			_( input ).each(function( value, key ) {
+				input[ key ] = +value
+			});
 
-			var scope = $rootScope.$new();
-			scope.dim = input;
+			var d = [],
+				array = glyphs[ glyph ]( input );
 
-			_( glyphs[ glyph ] ).each(function( line ) { 
-				d.push( line.join(' ').replace(/{{(.*?)}}/g, function( $0, $1 ) {
-					return scope.$eval( $1 );
-				}) );
+			_( array ).each(function( line ) { 
+				d.push( line.join(' ') );
 			});
 
 			return d.join(' ');
--- app/scripts/glyphs/i.js
+++ app/scripts/glyphs/i.js
@@ -1,21 +1,25 @@
 angular.module('prototyp0Glyph', ['prototyp0App'])
 	.run(function( glyphs ) {
-		glyphs['i'] = [
-			['M', '655', '{{650 - dim.hauteur}}'],
-			['L', '655', '281'],
-			['L', '{{534 - dim.largeur}}', '281'],
-			['L', '{{534 - dim.largeur}}', '{{308 -- dim.hauteur}}'],
-			['L', '{{534 - dim.largeur}}', '{{335 -- dim.hauteur}}'],
-			['L', '571', '{{335 -- dim.hauteur}}'],
-			['L', '571', '{{650 - dim.hauteur}}'],
-			['L', '{{534 - dim.largeur}}', '{{650 - dim.hauteur}}'],
-			['L', '{{534 - dim.largeur}}', '675'],
-			['L', '{{534 - dim.largeur}}', '703'],
-			['L', '612', '703'],
-			['L', '{{690 -- dim.largeur}}', '703'],
-			['L', '{{690 -- dim.largeur}}', '675'],
-			['L', '{{690 -- dim.largeur}}', '{{650 - dim.hauteur}}']
-		];
+		glyphs['i'] = function( dim ) {
+			var glyph = [];
+
+			glyph[0] = ['M', 655, 650 - dim.hauteur];
+			glyph[1] = ['L', 655, 281];
+			glyph[2] = ['L', 534 - dim.largeur, 281];
+			glyph[3] = ['L', 534 - dim.largeur, 308 + dim.hauteur];
+			glyph[4] = ['L', 534 - dim.largeur, 335 + dim.hauteur];
+			glyph[5] = ['L', 571, 335 + dim.hauteur];
+			glyph[6] = ['L', 571, 650 - dim.hauteur];
+			glyph[7] = ['L', 534 - dim.largeur, 650 - dim.hauteur];
+			glyph[8] = ['L', 534 - dim.largeur, 675];
+			glyph[9] = ['L', 534 - dim.largeur, 703];
+			glyph[10] = ['L', 612, 703];
+			glyph[11] = ['L', 690 + dim.largeur, 703];
+			glyph[12] = ['L', 690 + dim.largeur, 675];
+			glyph[13] = ['L', 690 + dim.largeur, 650 - dim.hauteur];
+
+			return glyph;
+		};
 
 		glyphs['H'] = [
 			['M', '347','127'],
