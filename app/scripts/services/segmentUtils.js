4ab1d0393bc66a308ad6cd169d0ce3862324ae9a
louisremi <lrbabe@gmail.com>
2013-06-29 15:31:59 +0200

    commit before tests

--- app/scripts/services/segmentUtils.js
+++ app/scripts/services/segmentUtils.js
@@ -3,8 +3,29 @@
 angular.module('prototyp0.segmentUtils', [])
 	.constant('segmentMethods', {})
 
-	.config(function( segmentMethods ) {
-		segmentMethods.find = function( params ) {
+	.factory('findOnBezier', function( segmentMethods ) {
+		return segmentMethods.findOnBezier = function( params ) {
+			var start = params.on[0],
+				end = params.on[1],
+				// intermediate points
+				tmp1 = {
+					x: start.x + ( end.controls[0].x - start.x ) * params.t,
+					y: start.y + ( end.controls[0].y - start.y ) * params.t,
+				},
+				tmp2 = {
+					x: start.x + ( end.controls[1].x - start.x ) * params.t,
+					y: start.y + ( end.controls[1].y - start.y ) * params.t,
+				};
+
+				//console.log( [].indeOf.apply( this.self, [params.on[0]] ) )
+				//control1 =
+			// find coordinates of a point at a given percentage on a bezier
+
+		};
+	})
+
+	.factory('findOnLineSegment', function( segmentMethods ) {
+		return segmentMethods.find = function( params ) {
 			var start = params.on[0],
 				end = params.on[1],
 				vector = {
@@ -18,15 +39,6 @@ angular.module('prototyp0.segmentUtils', [])
 			return point.join(' ');
 		};
 	})/*
-
-	.config('findOnBezier', function( params ) {
-		var start = params.on[0],
-			end = params.on[1];
-
-			console.log( [].indeOf.apply( this.self, [params.on[0]] ) )
-			//control1 =
-		// find coordinates of a point at a given percentage on a bezier
-	});
 
 	.config('pointOnStraightSegmentFromPercentage', function(start, end, percentage) {
 
