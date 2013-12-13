869f4e907e116f7e68da62fa86a1d9ffe522ba15
yannick mathey <hello@yannickmathey.com>
2013-12-13 21:03:52 +0100

    Add glyphs + guidelines
    + accent

---
+++ app/scripts/directives/guidelines.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('prototypo.guidelinesDirective', [])
+	.directive('guidelines', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'views/guidelines.html',
+			replace: true,
+			link: function postLink( $scope, $element ) {
+				
+				
+				
+			}
+
+		};
+	});
