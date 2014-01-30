d65f54518e31889f7718faa2e9ff079cca2c591a
louisremi <lrbabe@gmail.com>
2014-01-30 20:42:00 +0100

    Exclude handlebars templates in karma.conf.js + add custom version of jQuery with improved Pointer Events compatibility

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,9 @@ files = [
 ];
 
 // list of files to exclude
-exclude = [];
+exclude = [
+  'app/scripts/templates/*.js'
+];
 
 // test results reporter to use
 // possible values: dots || progress || growl
