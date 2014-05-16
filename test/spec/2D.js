39e4ecdabe629fa91f440602aa7ff4af73a8c65a
louisremi <lrbabe@gmail.com>
2014-05-16 16:52:30 +0200

    round some numbers in transformToMatrix2d to get test to pass in PhantomJS

--- test/spec/2D.js
+++ test/spec/2D.js
@@ -9,10 +9,10 @@ describe('2D utils', function () {
 		it('can convert a single rotate command to a matrix2d', inject(function( transformToMatrix2d ) {
 			var m = transformToMatrix2d( 'rotate(90)' );
 
-			expect( m[0] ).toBe(  0 );
+			expect( +m[0].toFixed(6) ).toBe(  0 );
 			expect( m[1] ).toBe(  1 );
 			expect( m[2] ).toBe( -1 );
-			expect( m[3] ).toBe(  0 );
+			expect( +m[3].toFixed(6) ).toBe(  0 );
 			expect( m[4] ).toBe(  0 );
 			expect( m[5] ).toBe(  0 );
 		}));
@@ -53,7 +53,7 @@ describe('2D utils', function () {
 		it('can convert multiple transform commands to a matrix2d', inject(function( transformToMatrix2d ) {
 			var m = transformToMatrix2d( 'translate(-100, -100) rotate(90) translate(100, 100) skewX(-45)' );
 
-			expect( m[0] ).toBe(  0 );
+			expect( +m[0].toFixed(6) ).toBe(  0 );
 			expect( m[1] ).toBe(  1 );
 			expect( m[2] ).toBe( -1 );
 			expect( m[3] ).toBe( -1 );
@@ -64,10 +64,10 @@ describe('2D utils', function () {
 		it('accepts a tranform-origin as a second parameter', inject(function( transformToMatrix2d ) {
 			var m = transformToMatrix2d( 'rotate(90)', {x: 50, y: -50} );
 
-			expect( m[0] ).toBe(  0 );
+			expect( +m[0].toFixed(6) ).toBe(  0 );
 			expect( m[1] ).toBe(  1 );
 			expect( m[2] ).toBe( -1 );
-			expect( m[3] ).toBe(  0 );
+			expect( +m[3].toFixed(6) ).toBe(  0 );
 			expect( m[4] ).toBe(  0 );
 			expect( m[5] ).toBe(  -100 );
 		}));
