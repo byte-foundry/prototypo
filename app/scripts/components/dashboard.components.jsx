387ac7d49162d52da23c019f7fa93011e8fa9ef4
FranzP <francois.poizat@gmail.com>
2015-04-29 18:33:51 +0200

    added dashboard components

---
+++ app/scripts/components/dashboard.components.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Sidebar from './sidebar.components.jsx';
+
+export default class Dashboard extends React.Component {
+
+	render() {
+
+		/* These are some guidelines about css:
+		 * - All these guidelines have to be considered in the scope of SMACSS
+		 * - All the first descendant of dashboard are unique layout container
+		 * (i.e they have a unique id in there first element preferrably the
+		 * lowercased name of the component)
+		 * - Layout component should be named with a Capitalized name
+		 * (i.e Sidebar, Menubar or Workboard)
+		 * - All descendant of layout components are modules
+		 * - the modules should have a class that is the name of the component
+		 * in kebab-case (YoYoMa -> yo-yo-ma);
+		 * - layout styles are prefixed with "l-"
+		 * - state styles are prefixed with "is-"
+		*/
+
+		return (
+			<div id="dashboard">
+				<Sidebar />
+			</div>
+		)
+	}
+}
