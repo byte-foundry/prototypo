e16dcac81314df3883e7005e579a909edd79702f
yannick mathey <hello@yannickmathey.com>
2015-09-01 11:16:04 +0200

    Date format with moment.js

--- app/scripts/components/commits-list.components.jsx
+++ app/scripts/components/commits-list.components.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Lifespan from 'lifespan';
 import LocalClient from '../stores/local-client.stores.jsx';
 import ClassNames from 'classnames';
+import moment from 'moment/min/moment-with-locales.min';
 
 export default class CommitsList extends React.Component {
 	componentWillMount() {
 		this.lifespan = new Lifespan();
 		this.client = new LocalClient.instance();
+
+		var locale = window.navigator.userLanguage || window.navigator.language;
+		moment.locale(locale)
 	}
 
 	componentWillUnmount() {
@@ -32,7 +36,7 @@ export default class CommitsList extends React.Component {
 					</div>
 				</h2>
 				<p className="news-feed-article-date">
-					{`${new Date(this.props.date)}`}
+					{`${moment(this.props.date).format('L')}`}
 				</p>
 				<div className="news-feed-article-content">
 					{results.map(function(result) {
