0fd49772f38dfd7aaac1f4c063e0a7b32ceedb89
= <lrbabe@gmail.com>
2015-09-09 20:42:24 +0200

    another attempt at fixing set.indexOf error

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -432,8 +432,9 @@ if ( isSafari || isIE ) {
 			});
 
 			const font = window.fontInstance = await fontPromise;
+			const subset = appValues.values.text + appValues.values.word;
 			await font.loadFont( typedata.fontinfo.familyName, typedataJSON );
-			font.subset = appValues.values.text + appValues.values.word;
+			font.subset = typeof subset === 'string' ? subset : '';
 			font.displayChar( appValues.values.selected );
 			localClient.dispatchAction('/create-font', font);
 
