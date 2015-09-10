4e1deb92f43aac2c9551c72bd832d9220146fa07
= <lrbabe@gmail.com>
2015-09-10 16:39:21 +0200

    You forgot to add the log.services.jsx file, silly you

---
+++ app/scripts/services/log.services.js
@@ -0,0 +1,14 @@
+// log things using google analytics events
+function Log( category, action, label ) {
+	if ( !window.ga ) {
+		return;
+	}
+
+	window.ga( 'send', 'event', category, action, label );
+}
+
+Log.ui = function( action, label ) {
+	Log( 'ui', action, label );
+};
+
+export default Log
