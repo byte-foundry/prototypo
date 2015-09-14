377cf21ef6f27c3ecebc96b03fb0f0fdce89dbe1
FranzPoize <francois.poizat@gmail.com>
2015-09-14 18:53:33 +0200

    whoops

--- app/scripts/services/commits.services.js
+++ app/scripts/services/commits.services.js
@@ -4,7 +4,7 @@ Commits.getCommits = (repo) => {
 	const xhr = new XMLHttpRequest();
 
 	return new Promise((resolve,reject) => {
-		xhr.open('GET','https://api.github.com/repos/byte-foundry/' + repo + '/commits?client_id=a416902f0b5e06b0e403&client_secret=0b1c56ee31a87aaa07ffb96f054d706ccedada8a');
+		xhr.open('GET','https://api.github.com/repos/byte-foundry/' + repo + '/commits');
 
 		xhr.onload = (e) => {
 			resolve(e.target.responseText);
