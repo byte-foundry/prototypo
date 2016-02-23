0f600eca6b1b64aaeaf869222f23d1ed7e7c1b82
FranzPoize <francois.poizat@gmail.com>
2016-02-23 19:12:16 +0100

    start of subscription adding option of nolabel for DisplayWithLabel

--- app/scripts/components/account/account-subscription.components.jsx
+++ app/scripts/components/account/account-subscription.components.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import DisplayWithLabel from '../shared/display-with-label.components.jsx';
+
 export default class AccountSubscription extends React.Component {
 	render() {
 		return (
-			<div className="account-subscription">
+			<div className="account-base account-subscription">
+				<DisplayWithLabel label="Your plan" data="Monthly professional subscription"/>
+				<p>
+					Your subscription will automatically renew on <span className="account-emphase">03/09/2016</span> and you will be charged <span className="account-emphase">$15</span>
+				</p>
+				<DisplayWithLabel label="Your card" data="card data"/>
+				<DisplayWithLabel nolabel={true} data="card data"/>
 			</div>
 		);
 	}
--- app/scripts/components/shared/display-with-label.components.jsx
+++ app/scripts/components/shared/display-with-label.components.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 export default class DisplayWithLabel extends React.Component {
 	render() {
+		const label = this.props.nolabel
+			? false
+			: <label className="display-with-label-label">{this.props.label}</label>;
+
 		return (
 			<div className="display-with-label">
-				<label className="display-with-label-label">{this.props.label}</label>
+				{label}
 				<div className="display-with-label-display">
 					{this.props.data}
 				</div>
