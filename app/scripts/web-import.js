1dd7b22ac0b62aed58bcd516c280d597054e5292
FranzPoize <francois.poizat@gmail.com>
2016-03-29 18:57:07 +0200

    tis working

---
+++ app/scripts/web-import.js
@@ -0,0 +1,33 @@
+import PrototypoCanvas from 'prototypo-canvas';
+
+const workerDeps = document.querySelector('script[src*=prototypo\\.]').src;
+let workerUrl;
+
+// The worker will be built from URL during development, and from
+// source in production.
+if (process.env.NODE_ENV !== 'production') {
+	workerUrl = '/prototypo-canvas/src/worker.js';
+}
+
+const canvas = document.getElementById('prototypo-canvas');
+
+const fontPromise = PrototypoCanvas.init({
+	canvas,
+	workerUrl,
+	workerDeps,
+	jQueryListeners: false,
+});
+
+fontPromise.then(function(data) {
+	const font = window.fontInstance = data;
+	data.worker.port.addEventListener('message', function(e) {
+		if (e.data instanceof ArrayBuffer) {
+			if (window.parent) {
+				window.parent.postMessage(e.data, '*');
+			}
+			else {
+				document.fonts.add(new FontFace('Prototypo Elzevir', e.data));
+			}
+		}
+	});
+});
