d42094c2e161240e79e42f7ccf2fa5074890bbf2
FranzPoize <francois.poizat@gmail.com>
2016-07-11 18:43:54 +0200

    fix indiv

--- app/scripts/helpers/appSetup.helpers.js
+++ app/scripts/helpers/appSetup.helpers.js
@@ -29,9 +29,9 @@ const defaultValues = {
 			onboard: false,
 			onboardstep: 'welcome',
 			word: 'Hello Prototypo',
-			wordFontSize: '7.6em',
+			wordFontSize: 7.6,
 			text: 'Type any text here and preview your modifications in real time! Curabitur blandit tempus porttitor. Maecenas sed diam eget risus varius blandit sit amet non magna. Sed posuere consectetur est at lobortis. Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla vitae elit libero, a pharetra augue. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Vestibulum id ligula porta felis euismod semper. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras mattis consectetur purus sit amet fermentum. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec sed odio dui.',
-			textFontSize: '1.6em',
+			textFontSize: 1.6,
 			pos: ['Point', 457, -364],
 			familySelected: {
 				name: 'My first font',
