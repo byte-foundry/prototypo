4259f5cc4afe9319d2fcc7631e52fa0bc32bede8
Martin Bolot <martin@martinbolot.com>
2016-07-22 14:16:39 +0200

    fixed credits overlay condition for free account

--- app/scripts/components/topbar/allowed-top-bar-with-payment.components.jsx
+++ app/scripts/components/topbar/allowed-top-bar-with-payment.components.jsx
@@ -4,8 +4,9 @@ export default class AllowedTopBarWithPayment extends React.Component {
 	render() {
 		const freeAccount = this.props.freeAccount;
 		const credits = this.props.credits;
+		console.log(freeAccount, credits);
 
-		const overlay = freeAccount && credits <= 0
+		const overlay = freeAccount && (!credits || credits <= 0)
 			? (
 				<div className="slider-demo-overlay">
 					<a className="slider-demo-overlay-text" href="#/account/credits">
