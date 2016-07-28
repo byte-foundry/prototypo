b1aefc184f12db78b8bbd65d5d15eb0baeadee4e
FranzPoize <francois.poizat@gmail.com>
2016-07-28 18:43:31 +0200

    added back uglifyjs

--- prod.config.js
+++ prod.config.js
@@ -55,6 +55,7 @@ module.exports = {
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': "'production'",
 		}),
+		new webpack.optimize.UglifyJsPlugin(),
 		new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.optimize.DedupePlugin(),
 	],
