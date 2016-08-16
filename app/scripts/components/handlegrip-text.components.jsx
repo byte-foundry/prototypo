ba878dafc878c32c63b18a522157fc7d24a3e1f3
Martin Bolot <martin@martinbolot.com>
2016-08-16 13:45:40 +0200

    first working version of drag n drop for spacing, must be improved

--- app/scripts/components/handlegrip-text.components.jsx
+++ app/scripts/components/handlegrip-text.components.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import LocalClient from '../stores/local-client.stores.jsx';
 import Lifespan from 'lifespan';
+import DOM from '../helpers/dom.helpers.js';
 
+/**
+*	Component : Text in prototypo-word
+*	Where you can set spacing on a specific letter
+*	@extends React.Component
+*/
 export default class HandlegripText extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			uiWordSelection: 0,
-			letterSpacing: 0,
+			letterSpacingLeft: 0,
+			letterSpacingRight: 0,
 		};
 		this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+
+		// function bindings
+		this.handleUp = this.handleUp.bind(this);
+		this.handleMove = this.handleMove.bind(this);
 	}
 
 	componentWillMount() {
@@ -21,8 +33,8 @@ export default class HandlegripText extends React.Component {
 			.onUpdate(({head}) => {
 				this.setState({
 					uiWordSelection: head.toJS().uiWordSelection || 0,
-					//letterSpacing: head.toJS().letterSpacing,
-					letterSpacing: 0,
+					letterSpacingLeft: head.toJS().letterSpacingLeft || 0,
+					letterSpacingRight: 0,
 				});
 			})
 			.onDelete(() => {
@@ -30,32 +42,85 @@ export default class HandlegripText extends React.Component {
 			});
 	}
 
+	componentDidMount() {
+		const handlegripDOM = ReactDOM.findDOMNode(this);
+
+		handlegripDOM.addEventListener('mouseup', this.handleUp);
+		handlegripDOM.addEventListener('mousemove', this.handleMove);
+	}
+
 	componentWillUnmount() {
 		this.lifespan.release();
 	}
 
-	/**
-	*	returns the letter to select (text at the right index)
-	*	@return {string} the letter
-	*/
-	getSelectedLetter() {
-		const selectedIndex = this.state.uiWordSelection;
+	handleUp(e) {
+		if (!this.refs.selectedLetter.tracking) {
+			return;
+		}
 
-		if (selectedIndex >= 0 && this.props.text && selectedIndex < this.props.text.length) {
-			return this.props.text[selectedIndex];
+		this.refs.selectedLetter.tracking = false;
+
+		/*
+		this.client.dispatchAction('/change-param', {
+			value: this.props.value,
+			name: this.props.name,
+			label: this.props.label,
+			force: true,
+		});
+		*/
+
+		e.stopPropagation();
+	}
+
+	handleMove(e) {
+		if (!this.refs.selectedLetter.tracking) {
+			return;
+		}
+
+		const newX = e.pageX || e.screenX;
+		const el = ReactDOM.findDOMNode(this);
+		const {offsetLeft} = DOM.getAbsOffset(el);
+		let newValue;
+
+		if (newX >= offsetLeft && newX <= offsetLeft + el.clientWidth) {
+			const variation = (
+				(newX - this.currentX) /* / this.sliderWidth  * (this.props.max - this.props.min)*/
+			);
+
+			newValue = /*this.props.value*/ this.state.letterSpacingLeft + variation;
+
+			// newValue = Math.min(Math.max(newValue, this.props.min), this.props.max);
 		}
 		else {
-			return null;
+			newValue = newX < offsetLeft ? /*this.props.min*/ 0 : /*this.props.max*/ 100;
 		}
+
+		/*
+		this.client.dispatchAction('/change-param', {
+			value: newValue,
+			name: this.props.name,
+		});
+		*/
+		this.client.dispatchAction('/store-value', {letterSpacingLeft: newValue});
+
+		this.currentX = newX;
 	}
 
 	render() {
-		const selectedLetter = this.getSelectedLetter();
-		const letterComponents = _.map(this.props.text, (letter, index) => {
+		const selectedLetter = this.state.uiWordSelection;
+		const letterComponents = _.map(this.props.text.split(''), (letter, index) => {
 			return (
-				selectedLetter === letter
+				selectedLetter === index
 				? (
-					<HandlegripLetter letter={letter} key={index}/>
+					<HandlegripLetter
+						letter={letter}
+						ref="selectedLetter"
+						spacingLeft={this.state.letterSpacingLeft}
+						spacingRight={this.state.letterSpacingRight}
+						min={0}
+						max={100}
+						key={index}
+					/>
 				)
 				: (
 					<RegularLetter letter={letter} key={index} index={index}/>
@@ -75,19 +140,69 @@ export default class HandlegripText extends React.Component {
 	}
 }
 
+/**
+*	Component : letter where you can set spacing
+*	With a drag'n'dropable handlegrip
+*	@extends React.Component
+*/
 class HandlegripLetter extends React.Component {
 	constructor(props) {
 		super(props);
 		this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+
+		// function bindings
+		this.handleDown = this.handleDown.bind(this);
+	}
+
+	handleDown(e) {
+		if (this.props.disabled) {
+			return;
+		}
+
+		this.tracking = true;
+
+		const newX = e.pageX || e.screenX;
+		const {offsetLeft} = DOM.getAbsOffset(this.refs.handlegripLeft);
+		let newValue = (
+			((newX - offsetLeft) /* / this.sliderWidth */ * (this.props.max - this.props.min))
+			+ this.props.min
+		);
+
+		newValue = Math.min(Math.max(newValue, this.props.min), this.props.max);
+
+		/*
+		this.client.dispatchAction('/change-param', {
+			value: newValue,
+			name: this.props.name,
+			label: this.props.label,
+		});
+		*/
+
+		this.currentX = newX;
+
+		e.stopPropagation();
 	}
 
 	render() {
+		const styleHandlegripLeft = {
+			left: this.props.spacingLeft,
+		};
+		const styleHandlegripRight = {
+			right: this.props.spacingRight,
+		};
+
 		return (
 			<span className="letter-wrap">
-				<span className="handlegrip handlegrip-left">
+				<span
+					className="handlegrip handlegrip-left"
+					ref="handlegripLeft"
+					onMouseDown={this.handleDown}
+					style={styleHandlegripLeft}
+				>
 					<span className="handlegrip-border"></span>
+					<span className="handlegrip-scale-left"></span>
 					<span className="handlegrip-spacing-number">
-						50
+						{this.props.spacingLeft}
 					</span>
 				</span>
 				<span className="letter-wrap-wrap">
@@ -97,13 +212,17 @@ class HandlegripLetter extends React.Component {
 					<span className="handlegrip-spacing-number">
 						450
 					</span>
-					<span className="handlegrip-scale-left"></span>
-					<span className="handlegrip-scale-right"></span>
 				</span>
-				<span className="handlegrip handlegrip-right">
+				<span
+					className="handlegrip handlegrip-right"
+					ref="handlegripRight"
+					onMouseDown={this.handleDown}
+					style={styleHandlegripRight}
+				>
 					<span className="handlegrip-border"></span>
+					<span className="handlegrip-scale-right"></span>
 					<span className="handlegrip-spacing-number">
-						50
+						{this.props.spacingRight}
 					</span>
 				</span>
 			</span>
@@ -111,6 +230,10 @@ class HandlegripLetter extends React.Component {
 	}
 }
 
+/**
+*	Component : a regular letter
+*	@extends React.Component
+*/
 class RegularLetter extends React.Component {
 	constructor(props) {
 		super(props);
