baf8e04582dc4dfd554795914ee8252e33b62c8d
yannick mathey <hello@yannickmathey.com>
2016-11-30 11:04:51 +0100

    coupon v2 launch offer

--- app/scripts/stores/creation.stores.jsx
+++ app/scripts/stores/creation.stores.jsx
@@ -238,6 +238,7 @@ const couponStore = new Remutable({
 	'de7ce5e5ee6d2527aa5aa0d1f624e704': {label: '50% off your annual subscription - Domestika discount'},
 	'14a6c65efc3f285ea0b2f45d75f73bde': {label: 'The first month for $5!'},
 	'b6f30aee2d4b895d61a2f72e6901c4e0': {label: 'Birthday offer, your first month for $1!'},
+	'131c543d598049843d548a0599b65491': {label: 'V2 launch offer, your first month for $1!'},
 });
 
 const planStore = new Remutable({
