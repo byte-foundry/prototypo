16861ad185d711fe85bce6e8a97b836dcac78fdf
Julien Barret <ibarretj@gmail.com>
2017-04-04 15:51:11 +0200

    account pages redesign

--- app/scripts/components/account/account-dashboard.components.jsx
+++ app/scripts/components/account/account-dashboard.components.jsx
@@ -4,6 +4,7 @@ import AccountSidebar from './account-sidebar.components.jsx';
 
 export default class AccountDashboard extends React.Component {
 	render() {
+		console.log(this.props);
 		const titles = {
 			home: "My account",
 			profile: "My account",
@@ -16,17 +17,36 @@ export default class AccountDashboard extends React.Component {
 			confirm: "My account",
 			billing: "My account",
 		};
+		const subtitles = {
+			home: "My account",
+			profile: "My profile",
+			'change-password': "Change my password",
+			details: "My account settings",
+			create: "",
+			createSignup: "",
+			signup: "",
+			signin: "",
+			success: "",
+			confirm: "",
+			billing: "My billing history",
+			'add-card': "Add a card",
+		};
 		const title = titles[this.props.route.name];
+		const subtitle = subtitles[this.props.location.pathname.split('/')[this.props.location.pathname.split('/').length - 1]];
 
 		return (
 			<div className="account-dashboard">
 				<Link to="/dashboard">
 					<div className="account-dashboard-icon"/>
 				</Link>
-				<Link to="/dashboard" className="account-dashboard-back-icon"/>
 				<div className="account-header">
 					<h1 className="account-title">{title}</h1>
 				</div>
+				{
+					subtitle === ""
+					? false
+					: (<h1 className="account-dashboard-page-title">{subtitle}</h1>)
+				}
 				<div className="account-dashboard-container">
 					<AccountSidebar />
 					{this.props.children}
