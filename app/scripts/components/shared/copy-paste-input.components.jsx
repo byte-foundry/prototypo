e76debe21be7aac933eb9c11e07a9e7e0fd1cf2c
FranzPoize <francois.poizat@gmail.com>
2017-09-06 14:39:44 +0200

    added copy to clipboard behavior and fix eslint issues

--- app/scripts/components/shared/copy-paste-input.components.jsx
+++ app/scripts/components/shared/copy-paste-input.components.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Button from './new-button.components.jsx';
-import InputWithLabel from './input-with-label.components.jsx';
+import Button from './new-button.components';
+import InputWithLabel from './input-with-label.components';
 
 export default class CopyPasteInput extends React.PureComponent {
+	constructor(props) {
+		super(props);
+		this.copyToClipboard = this.copyToClipboard.bind(this);
+		this.state = {
+			message: 'Copy to clipboard',
+		};
+	}
+
+	copyToClipboard() {
+		this.input.select();
+		try {
+			document.execCommand('copy');
+			this.setState({
+				message: 'Copied to clipboard',
+			});
+		}
+		catch (err) {
+			this.setState({
+				message: 'Can\'t copy :(',
+			});
+		}
+	}
+
 	render() {
 		const {content} = this.props;
 
@@ -12,10 +35,21 @@ export default class CopyPasteInput extends React.PureComponent {
 			<div className="copy-paste-input">
 				<div className="columns">
 					<div className="two-third-column">
-						<InputWithLabel inputValue={content} size="small"/>
+						<InputWithLabel
+							inputValue={content}
+							size="small"
+							inputRef={
+								(input) => {
+									this.input = input;
+								}
+							}
+						/>
 					</div>
 					<div className="third-column">
-						<Button size="small" onClick={() => {}}>Copy to clipboard</Button>
+						<Button
+							size="small"
+							onClick={this.copyToClipboard}
+						>{this.state.message}</Button>
 					</div>
 				</div>
 			</div>
@@ -25,4 +59,4 @@ export default class CopyPasteInput extends React.PureComponent {
 
 CopyPasteInput.propTypes = {
 	content: PropTypes.string,
-}
+};
