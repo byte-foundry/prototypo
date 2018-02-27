a7d2f816d455b7291b5be38ea3ca8b6ebcc04870
FranzPoize <francois.poizat@gmail.com>
2018-02-27 16:10:21 +0100

    spacing in canvas

--- app/scripts/components/handlegrip/handlegrip-bar.components.jsx
+++ app/scripts/components/handlegrip/handlegrip-bar.components.jsx
@@ -27,6 +27,9 @@ export default class HandlegripBar extends React.Component {
 	}
 
 	handleDown(e) {
+		if (this.props.noInteraction) {
+			return;
+		}
 		// tells everyone that the tracking begins, and on which side
 		this.client.dispatchAction('/store-value', {uiSpacingTracking: this.props.side});
 
@@ -45,7 +48,10 @@ export default class HandlegripBar extends React.Component {
 			'handlegrip-right': !left,
 		});
 
-		const text = (this.props.clampedValue ? this.props.clampedValue + this.props.baseSpacing : this.props.spacing) || '...';
+		let text = (this.props.clampedValue === undefined ? this.props.spacing : this.props.clampedValue + this.props.baseSpacing) || '...';
+
+		text = text instanceof Number ? text : parseInt(text);
+		const offset = text - this.props.baseSpacing;
 
 		return (
 			<span
@@ -55,7 +61,7 @@ export default class HandlegripBar extends React.Component {
 			>
 				<span className="handlegrip-border" />
 				<span className="handlegrip-spacing-number">
-					{text instanceof Number ? text.toFixed(0) : parseInt(text).toFixed(0)}
+					{text.toFixed(0)} ({offset})
 				</span>
 			</span>
 		);
