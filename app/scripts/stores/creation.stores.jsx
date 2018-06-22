b74fdb1c41ee38a3806f087aad88926c40bad479
Alexis Launay <yorunohikage@gmail.com>
2018-06-22 15:03:12 +0200

    Add undoable guides

--- app/scripts/stores/creation.stores.jsx
+++ app/scripts/stores/creation.stores.jsx
@@ -3,6 +3,7 @@ import Remutable from 'remutable';
 const undoableStore = new Remutable({
 	// font controls store values
 	controlsValues: {},
+	guides: [],
 	// end font controls store values
 });
 
@@ -170,7 +171,7 @@ const prototypoStore = new Remutable({
 	// end indiv store values
 
 	// intercom store values
-	intecomTags: undefined,
+	intercomTags: undefined,
 	// end intercom store values
 
 	// search store values
