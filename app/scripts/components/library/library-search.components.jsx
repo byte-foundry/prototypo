550dda8bedf7d25429a96ab5e0aae6073f362ff0
Julien Barret <ibarretj@gmail.com>
2018-07-11 11:01:27 +0200

    Basic search system

--- app/scripts/components/library/library-search.components.jsx
+++ app/scripts/components/library/library-search.components.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Lifespan from 'lifespan';
 
 import LocalClient from '../../stores/local-client.stores';
@@ -8,36 +7,24 @@ export default class LibrarySearch extends React.PureComponent {
 	constructor(props) {
 		super(props);
 		this.state = {};
-		this.onChange = this.onChange.bind(this);
 	}
 
 	componentWillMount() {
 		this.client = LocalClient.instance();
-		this.lifespan = new Lifespan();
-		this.client
-			.getStore('/prototypoStore', this.lifespan)
-			.onUpdate((head) => {
-				this.setState({
-					search: head.toJS().d.librarySearchString,
-				});
-			})
-			.onDelete(() => {
-				this.setState(undefined);
-			});
-	}
-
-	onChange(e) {
-		this.setState({
-			search: e.target.value,
-		});
-		this.client.dispatchAction('/store-value', {
-			search: e.target.value,
-		});
 	}
 
 	render() {
 		return (
-			<input type="text" placeholder="Search" value={this.state.search} onChange={this.onChange}/>
+			<div className="library-search">
+				<input type="text" placeholder="Search" value={this.state.search} onChange={(e) => {
+					this.setState({
+						search: e.target.value,
+					});
+					this.client.dispatchAction('/store-value', {
+						librarySearchString: e.target.value,
+					});
+				}}/>
+			</div>
 		);
 	}
 }
