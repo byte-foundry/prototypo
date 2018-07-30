23bc96e8fa6ce9fd83bdd5401ff49f09cfce8e2b
Alexis Launay <yorunohikage@gmail.com>
2018-07-30 20:13:47 +0200

    Add link and big option for Button
    - link is a boolean used to change the appearance of the Button to make it as a link,
      the same way it is on Bootstrap for example
    - big is a new type of the size prop that is a in-between regular and large
    - Change the default behavior of the button to be type button instead of submit, this
      will avoid validating forms by default since we usually put a click handler on it.
      To set the default behavior back, just set it to type="submit".

--- __tests__/Storyshots.js
+++ __tests__/Storyshots.js
@@ -1,3 +1,9 @@
 import initStoryshots from '@storybook/addon-storyshots';
 
+global.window = global;
+window.addEventListener = () => {};
+window.requestAnimationFrame = () => {
+	throw new Error('requestAnimationFrame is not supported in Node');
+};
+
 initStoryshots();
